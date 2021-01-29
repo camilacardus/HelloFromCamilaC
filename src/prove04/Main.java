7b174cbf83bd95f5d6be7e86c91110e83cd1fe0b
Camila C <cardus4@churchofjesuschrist.org>
2021-01-29 17:20:21 -0300

    Initial commit

---
+++ src/prove04/Main.java
@@ -0,0 +1,13 @@
+package prove04;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main Hello = new Main();
+        Hello.output();
+    }
+
+    public void output() {
+        System.out.println("Hello, my name is Camila C");
+    }
+}
