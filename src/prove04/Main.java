7aef766502fca1011de8e35f1b0b31977c9fca42
Gabriel Cánepa <gacanepa@gmail.com>
2021-01-29 21:58:32 -0300

    Note from Gabriel

--- src/prove04/Main.java
+++ src/prove04/Main.java
@@ -5,9 +5,14 @@ public class Main {
     public static void main(String[] args) {
         Main Hello = new Main();
         Hello.output();
+        Hello.gabrielSaysHi();
     }
 
     public void output() {
         System.out.println("Hello, my name is Camila C");
+    }
+
+    public void gabrielSaysHi() {
+        System.out.println("Good job here!");
     }
 }
